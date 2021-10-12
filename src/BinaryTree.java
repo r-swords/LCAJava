fdc758f4145bf10fac0bef501c047d14971a7b55
ralph1 <rswords@tcd.ie>
2021-10-12 20:25:32 +0100

    Removed main

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -36,16 +36,4 @@ public class BinaryTree {
 
     }
 
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
-        tree.root.right = new Node(2);
-        tree.root.left = new Node(3);
-        tree.root.right.right = new Node(4);
-        tree.root.right.left = new Node(5);
-        tree.root.left.right = new Node(6);
-        tree.root.left.left = new Node(7);
-        System.out.println(tree.findLca(4, 5).data);
-        if(tree.findLca(4,10) == null) System.out.println("not present");
-    }
 }
