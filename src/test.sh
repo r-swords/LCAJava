16036551e1a7968da7be1a5f07a314f6b6b616b7
Ralph Swords <91212810+r-swords@users.noreply.github.com>
2021-10-27 12:30:48 +0100

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 mkdir out
-javac -d out BinaryTree.java
-javac -d out -cp out:junit-platform-console-standalone-1.7.0-all.jar BinaryTreeTest.java
+javac -d out DAG.java
+javac -d out -cp out:junit-platform-console-standalone-1.7.0-all.jar DAGTest.java
 java -jar junit-platform-console-standalone-1.7.0-all.jar --class-path out --scan-class-path
 rm -r out
