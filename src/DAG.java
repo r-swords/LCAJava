14d4b9da832a7dd294f5c1f6186d0ee87b0b521f
Ralph Swords <91212810+r-swords@users.noreply.github.com>
2021-10-27 12:29:52 +0100

    Update DAG.java

--- src/DAG.java
+++ src/DAG.java
@@ -4,29 +4,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class DAG {
-    LinkedList<Integer>[] graph;
-    LinkedList<Integer>[] parents;
+    ArrayList<LinkedList<Integer>> graph;
+    ArrayList<LinkedList<Integer>> parents;
 
     DAG(int size){
-        graph = new LinkedList[size];
-        parents = new LinkedList[size];
+        graph = new ArrayList<>(size);
+        parents = new ArrayList<>(size);
         for(int i = 0; i < size; i++){
-            graph[i] = new LinkedList<Integer>();
-            parents[i] = new LinkedList<Integer>();
+            graph.set(i, new LinkedList<>());
+            parents.set(i, new LinkedList<>());
         }
     }
 
     public void addEdge(int u, int v){
-        graph[u].add(v);
-        parents[v].add(u);
+        graph.get(u).add(v);
+        parents.get(v).add(u);
     }
 
     public ArrayList<Integer> findLCA(int u, int v) {
-        if(u >= graph.length || v >= graph.length || u < 0 || v <0 || u == v) return null;
-        ArrayList<Integer> lca = new ArrayList<Integer>();
+        if(u >= graph.size() || v >= graph.size() || u < 0 || v <0 || u == v) return null;
+        ArrayList<Integer> lca = new ArrayList<>();
         int maxDist = Integer.MAX_VALUE;
         int uDist, vDist;
-        for(int i = 0; i < graph.length; i++)
+        for(int i = 0; i < graph.size(); i++)
         {
             if(isParent(i, v) && isParent(i, u))
             {
@@ -34,7 +34,7 @@ public class DAG {
                 vDist = getDistance(i, v);
                 if(Integer.max(uDist, vDist) < maxDist)
                 {
-                    lca = new ArrayList<Integer>();
+                    lca = new ArrayList<>();
                     lca.add(i);
                     maxDist = Integer.max(uDist, vDist);
                 }
@@ -50,8 +50,8 @@ public class DAG {
     }
 
     private boolean isParent(int u, int v){
-        if(graph[u].contains(v)) return true;
-        for(int i: graph[u]){
+        if(graph.get(u).contains(v)) return true;
+        for(int i: graph.get(u)){
             if(isParent(i,v)) return true;
         }
         return false;
@@ -59,7 +59,7 @@ public class DAG {
 
     private int getDistance(int u, int v){
         Queue<Integer> bfsQueue = new LinkedList<>();
-        int[] distances = new int[graph.length];
+        int[] distances = new int[graph.size()];
         if(u == v) return 0;
         else{
             bfsQueue.add(u);
@@ -67,7 +67,7 @@ public class DAG {
             distances[u] = 0;
             while(!bfsQueue.isEmpty()){
                 int i = bfsQueue.remove();
-                for(int j: graph[i]){
+                for(int j: graph.get(i)){
                     if(distances[j] == -1) {
                         bfsQueue.add(j);
                         distances[j] = distances[i] + 1;
